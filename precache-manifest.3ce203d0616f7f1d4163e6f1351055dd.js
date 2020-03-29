0834b8c5baf14c342fb7094f491403e36b0a722b
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-03-29 15:05:43 +0100

    Updates

---
+++ precache-manifest.3ce203d0616f7f1d4163e6f1351055dd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85630f8c37e9037c4f584b871ba57de",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "c8fe99e6e7f67f0c0607",
+    "url": "/ReactApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "3f9d8066a0a17c46d70a",
+    "url": "/ReactApp/static/css/main.7fbf2311.chunk.css"
+  },
+  {
+    "revision": "c8fe99e6e7f67f0c0607",
+    "url": "/ReactApp/static/js/2.347054ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.347054ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9d8066a0a17c46d70a",
+    "url": "/ReactApp/static/js/main.6ac038ab.chunk.js"
+  },
+  {
+    "revision": "4f9a005907a8fa3bcb7d",
+    "url": "/ReactApp/static/js/runtime-main.8193f0d0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9721e8ec2ef0dd0df0b1e30873f02a29",
+    "url": "/ReactApp/static/media/ahri.9721e8ec.jpg"
+  },
+  {
+    "revision": "84749ed254b10216b041cc8bf1b7fc4f",
+    "url": "/ReactApp/static/media/ashe.84749ed2.jpg"
+  },
+  {
+    "revision": "e443c5b9e5ab95f9b11066b42a221a27",
+    "url": "/ReactApp/static/media/ekko.e443c5b9.jpg"
+  },
+  {
+    "revision": "fe3d7267256b26bb6d3c4c5448cfe47f",
+    "url": "/ReactApp/static/media/ezreal.fe3d7267.jpg"
+  },
+  {
+    "revision": "e33abeb54c605bdfa3b175e376696ca0",
+    "url": "/ReactApp/static/media/jayce.e33abeb5.jpg"
+  },
+  {
+    "revision": "1916b8e05a0a0904f9a93c2debcc4f0f",
+    "url": "/ReactApp/static/media/jhin.1916b8e0.jpg"
+  },
+  {
+    "revision": "105be1ca53620a058a9b74067ba80422",
+    "url": "/ReactApp/static/media/jinx.105be1ca.jpg"
+  },
+  {
+    "revision": "b8d987c3fc55fb17a6c525d288c99525",
+    "url": "/ReactApp/static/media/kaisa.b8d987c3.jpg"
+  },
+  {
+    "revision": "5782306516ae0cb5dc35414c3aeba251",
+    "url": "/ReactApp/static/media/kayle.57823065.jpg"
+  },
+  {
+    "revision": "9594f31dc1209f0855d6c900439981a5",
+    "url": "/ReactApp/static/media/lucian.9594f31d.jpg"
+  },
+  {
+    "revision": "22ee4042f56a60a43930ec250b92de64",
+    "url": "/ReactApp/static/media/tft.22ee4042.jpg"
+  },
+  {
+    "revision": "067518736cf5f611f09aab28f3171759",
+    "url": "/ReactApp/static/media/yasuo.06751873.jpg"
+  },
+  {
+    "revision": "7af61b151398b79d0765fcb3aedeb8cc",
+    "url": "/ReactApp/static/media/ziggz.7af61b15.jpg"
+  }
+]);
--- src/components/Scroll.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Scroll = (props) => {
-    return (<div >
-        {props.children}
-    </div>)
-};
-export default Scroll;
