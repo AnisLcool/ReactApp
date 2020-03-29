774bddb738865bcb3c89e8238288afc81d48ffc2
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-03-29 14:14:33 +0100

    fixed bug images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css';
 import 'tachyons';
-const Card_Component = ({name, origin, classs, tier}) => {
-    
-    return (
 
+const Card_Component = ({name, origin, classs, tier}) => {
+    return (
         <div className="galactic dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`${name}.jpg`} alt="lol_champ" width="200px" height="340px"/>
+            <img src={require(`../${name.toLowerCase()}.jpg`)} alt="lol_champ" width="200px" height="340px"/>
+
             <div>
                 <h2>{name}</h2>
                 <p className="origin">Origin ~ {origin}</p>
